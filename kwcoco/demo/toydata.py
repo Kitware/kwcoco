ef0864b1273fd362090ddba9526c1c97ec87ea72
joncrall <jon.crall@kitware.com>
2021-04-06 21:41:27 -0400

    gdal 3.2.1 fix

--- kwcoco/demo/toydata.py
+++ kwcoco/demo/toydata.py
@@ -206,7 +206,7 @@ def demodata_toy_dset(gsize=(600, 600), n_imgs=5, verbose=3, rng=0,
                 kpname_to_id[kpcat['name']] = kpcat['id']
 
         try:
-            import gdal  # NOQA
+            from osgeo import gdal  # NOQA
         except Exception:
             imwrite_kwargs = {}
         else:
@@ -1105,7 +1105,7 @@ def render_toy_dataset(dset, rng, dpath=None, renderkw=None):
                 auxdict['file_name'] = aux_fpath
                 auxdata = auxdict.pop('imdata', None)
                 try:
-                    import gdal  # NOQA
+                    from osgeo import import gdal  # NOQA
                     kwimage.imwrite(aux_fpath, auxdata, backend='gdal', space=None)
                 except Exception:
                     kwimage.imwrite(aux_fpath, auxdata, space=None)
