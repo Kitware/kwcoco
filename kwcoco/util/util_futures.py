6f8136130d99774be9380a9524fa03d6fa828237
joncrall <erotemic@gmail.com>
2021-08-23 10:30:31 -0400

    Fix tests

--- kwcoco/util/util_futures.py
+++ kwcoco/util/util_futures.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 # Tools were ported to ubelt
-from ubelt import Executor, SerialExecutor, JobPool
-__all__ = ['Executor', 'SerialExecutor', 'JobPool']
+from ubelt import Executor, JobPool
+__all__ = ['Executor', 'JobPool']
