00b50158d516c0547e4a3d7c8182411b68bea03a
joncrall <erotemic@gmail.com>
2023-06-15 16:58:26 -0400

    wip

---
+++ kwcoco/cli/coco_move_assets.pyi
@@ -0,0 +1,13 @@
+import scriptconfig as scfg
+from _typeshed import Incomplete
+
+
+class CocoMoveAssetsCLI(scfg.DataConfig):
+    src: Incomplete
+    dst: Incomplete
+    io_workers: Incomplete
+    coco_fpaths: Incomplete
+
+
+def main(cmdline: int = ..., **kwargs) -> None:
+    ...
