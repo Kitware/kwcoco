e6c63ec8028024702d037e0875854c5092c86018
joncrall <erotemic@gmail.com>
2024-08-27 19:20:47 -0400

    Remove legacy config code

--- kwcoco/cli/__main__.py
+++ kwcoco/cli/__main__.py
@@ -55,26 +55,7 @@ def main(cmdline=True, **kw):
     for cli_module in cli_modules:
 
         cli_config = None
-        if hasattr(cli_module, '_CLI'):
-            # Old way
-            cli_cls = cli_module._CLI
-            cli_cls.CLIConfig.__command__ = cli_cls.name
-            assert hasattr(cli_cls, 'CLIConfig'), (
-                'We are only supporting scriptconfig CLIs')
-            # scriptconfig cli pattern
-            cli_config = cli_cls.CLIConfig
-
-            if not hasattr(cli_config, 'main'):
-                if hasattr(cli_cls, 'main'):
-                    main_func = cli_cls.main
-                    # Hack the main function into the config
-                    cli_config.main = main_func
-                else:
-                    raise AssertionError(f'No main function for {cli_module}')
-        elif hasattr(cli_module, '__config__'):
-            # New way
-            cli_config = cli_module.__config__
-        elif hasattr(cli_module, '__cli__'):
+        if hasattr(cli_module, '__cli__'):
             # New way
             cli_config = cli_module.__cli__
         else:
